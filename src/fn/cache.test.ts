8de277d742ea890036c674e7597328dd4f59bfba
timothy_michael <tim.everington@gmail.com>
2020-01-22 21:29:43 +0000

    weakMap for objs & simplify/fix for async

--- src/fn/cache.test.ts
+++ src/fn/cache.test.ts
@@ -15,14 +15,6 @@ describe('cache()', function () {
 		assert.notStrictEqual(nufn, testFn)
 	})
 
-	it('Retuns new async function', function () {
-		const testFn = async (a: number, b: number, c: number): Promise<number> => a + b + c
-		const nufn = cache(testFn)
-		assert.isFunction(nufn)
-		assert.strictEqual(toString.call(nufn), '[object AsyncFunction]')
-		assert.notStrictEqual(nufn, testFn)
-	})
-
 	it('Retuns same function when passed a generator', function () {
 		const testFn = function* (a: number, b: number, c: number) { yield a + b + c }
 		const nufn = cache(testFn)
@@ -47,42 +39,17 @@ describe('cache()', function () {
 	})
 
 	it('Returns the same output as the orignal function - mixed', function () {
-		const testFn = (a: { a: number, b: number, c: number }, d: number, e: any, f: number): number => a.a + a.b + a.c + d + f
+		const testFn = (a: { a: number, b: number, c: number }, d: number, e: any, f: number, g: any): number => a.a + a.b + a.c + d + f
 		const nufn = cache(testFn)
 		const obj = { a: 1, b: 2, c: 3 }
-		assert.strictEqual(testFn(obj, 1, null, 2), nufn(obj, 1, null, 2))
-		assert.strictEqual(testFn(obj, 1, null, 2), nufn(obj, 1, null, 2))
+		assert.strictEqual(testFn(obj, 1, null, 2, undefined), nufn(obj, 1, null, 2, undefined))
+		assert.strictEqual(testFn(obj, 1, null, 2, undefined), nufn(obj, 1, null, 2, undefined))
 	})
 
 	it('Returns a promise when caching an async function', async function () {
 		const testFn = async (a: number, b: number, c: number = 0): Promise<number> => a + b + c
 		const nufn = cache(testFn)
 		assert.ok(nufn(1,2,3) instanceof Promise)
-	})
-
-	it('Returns the same output as the orignal function - args - async', async function () {
-		const testFn = async (a: number, b: number, c: number = 0): Promise<number> => a + b + c
-		const nufn = cache(testFn)
-		assert.strictEqual(await testFn(1, 2, 3), await nufn(1, 2, 3))
-		assert.strictEqual(await testFn(1, 2, 3), await nufn(1, 2, 3))
-		assert.strictEqual(await testFn(10, 20), await nufn(10, 20))
-		assert.strictEqual(await testFn(10, 20), await nufn(10, 20))
-	})
-
-	it('Returns the same output as the orignal function - obj - async', async function () {
-		const testFn = async (a: { a: number, b: number, c: number }): Promise<number> => a.a + a.b + a.c
-		const nufn = cache(testFn)
-		const obj = { a: 1, b: 2, c: 3 }
-		assert.strictEqual(await testFn(obj), await nufn(obj))
-		assert.strictEqual(await testFn(obj), await nufn(obj))
-	})
-
-	it('Returns the same output as the orignal function - mixed - async', async function () {
-		const testFn = async (a: { a: number, b: number, c: number }, d: number, e: any, f: number): Promise<number> => a.a + a.b + a.c + d + f
-		const nufn = cache(testFn)
-		const obj = { a: 1, b: 2, c: 3 }
-		assert.strictEqual(await testFn(obj, 1, null, 2), await nufn(obj, 1, null, 2))
-		assert.strictEqual(await testFn(obj, 1, null, 2), await nufn(obj, 1, null, 2))
 	})
 
 })
--- src/fn/cache.ts
+++ src/fn/cache.ts
@@ -1,7 +1,6 @@
 /* eslint require-atomic-updates: off */
-// above rule :off due to line in nufnAsync:
-// if (!(key in resultCache)) resultCache[key] = await fn(...args)
-// This is not a race condition that matters!
+
+import { isScalar } from '../is/isScalar'
 
 export interface Icache {
 	/**
@@ -12,75 +11,43 @@ export interface Icache {
 		(...args: any[]): any
 	}
 }
-const CACHE_KEY_NAME = '__FN_CACHE_KEY__'
+
 export const cache: Icache = (fn) => {
-
 	// the cache
-	let resultCache: {
+	const resultCache: {
 		[index: string]: any
-	} = {}
+	}= {}
 
-	// unique ID for Object type arguments
-	// this id will be stored as a non-enumerable, non writable property of the object
-	let objCacheId = 0
+	// objects will map to an Id which is then used for resultCache key
+	const objKeyMap = new WeakMap()
+	let objKeyMapId = 0
 
 	// determins if we should return an asyn function or not.
 	let type = toString.call(fn)
 
-	// The returned functions, nufn and nufnAsync are mostly the same as each other.
-	// For some reason breaking out the 'get key' in to a  separate function has a massive impact on performance
+	// do not cache generator output!
+	if (type.indexOf('Generator') > -1) return fn
 
 	const nufn = (...args: any[]) => {
 		let key = ''
-		// get key
 		for (let i in args) {
-			if (!args[i]) {
+
+			if (isScalar(args[i]) || !args[i]) {
 				key += args[i]
 
-			} else if (typeof args[i] === 'object') {
-				if (args[i] && !args[i][CACHE_KEY_NAME]) {
-					Object.defineProperty(args[i], CACHE_KEY_NAME, {
-						value: ++objCacheId
-					})
-				}
-				key += args[i][CACHE_KEY_NAME]
+			} else if (objKeyMap.has(args[i])) {
+				key += objKeyMap.get(args[i])
+
 			} else {
-				key += String(args[i])
+				const nuKey = ++objKeyMapId
+				objKeyMap.set(args[i], objKeyMapId)
+				key += nuKey
 			}
 		}
 
 		if (!(key in resultCache)) resultCache[key] = fn(...args)
 		return resultCache[key]
 	}
-
-	const nufnAsync = async (...args: any[]) => {
-		let key = ''
-		// get key
-		for (let i in args) {
-			if (!args[i]) {
-				key += args[i]
-
-			} else if (typeof args[i] === 'object') {
-				if (args[i] && !args[i][CACHE_KEY_NAME]) {
-					Object.defineProperty(args[i], CACHE_KEY_NAME, {
-						value: ++objCacheId
-					})
-				}
-				key += args[i][CACHE_KEY_NAME]
-			} else {
-				key += String(args[i])
-			}
-		}
-
-		if (!(key in resultCache)) resultCache[key] = await fn(...args)
-		return resultCache[key]
-	}
-
-	// do not cache generator output!
-	if (type.indexOf('Generator') > -1) return fn
-
-	// return async fn
-	if (type.indexOf('Async') > -1) return nufnAsync
 
 	return nufn
 }
